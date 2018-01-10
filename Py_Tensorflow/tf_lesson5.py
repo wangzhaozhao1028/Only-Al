c4002dbe780c17cfce5a86c2d43816148c9ad376
t-wangzhaozhao <t-wangzhaozhao@megvii.com>
2018-01-10 19:16:09 +0800

    add lesson5 code

--- Py_Tensorflow/tf_lesson5.py
+++ Py_Tensorflow/tf_lesson5.py
@@ -1,41 +1,39 @@
 #coding=utf-8
 
-
-# 谷歌开发/神经网络
-# 参考官方文档
-"""lesson 4 code
+"""lesson 5 code
+	例子2
 """
+
+
 import tensorflow as tf 
 import numpy as np 
- 
+
+
  #create data
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data*0.1 + 0.3
 
 ### create tensorflow structure start ###
+# Weights 可能是个矩阵,大写容易辨别
+# 从初始值不断到预测状态
 Weights = tf.Variable(tf.random_uniform([1],-1.0,1.0))
 biases = tf.Variable(tf.zeros([1]))
 
-
 y = Weights*x_data + biases
-
+# 预测值与真实值的差距
 loss = tf.reduce_mean(tf.square(y-y_data))
-#优化器
 optimizer = tf.train.GradientDescentOptimizer(0.5)
-
 train = optimizer.minimize(loss)
 
-#初始化
+ # 初始化,变量都生效,结构激活
 init = tf.initialize_all_variables()
 ### create tensorflow structure end ###
-
+# session类似于指针,
 sess = tf.Session()
-sess.run(init)    #激活       Very important
-
+sess.run(init)
 
 for step in range(201):
+	# 真正训练
 	sess.run(train)
 	if step % 20 == 0:
-		print(step,sess.run(Weights),sess.run(biases))
-
-
+		print (step,sess.run(Weights),sess.run(biases))
